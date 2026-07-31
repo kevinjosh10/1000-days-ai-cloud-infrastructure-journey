8338fdf83cf79742b025aea85fc418e39033e005
Kevin Joshua <kingkevinjosh7@gmail.com>
2026-07-31 18:02:02 +0530

    feat(alb): Define inputs for Load Balancer ports and health checks

---
+++ projects/terraform/aws-3tier-architecture/modules/alb/variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_id" { type = string }
+variable "public_subnet_ids" { type = list(string) }
+variable "target_port" { type = number, default = 80 }
+variable "health_check_path" { type = string, default = "/" }
