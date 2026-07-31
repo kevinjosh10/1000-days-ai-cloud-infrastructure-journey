5e0d4203922d95a02f0f238fff84919b29305561
Kevin Joshua <kingkevinjosh7@gmail.com>
2026-07-31 18:02:01 +0530

    feat(vpc): Implement dynamic subnets, IGW, and VPC networking logic

---
+++ projects/terraform/aws-3tier-architecture/modules/vpc/main.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = { Name = "project1-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+  tags = { Name = "public-subnet-${count.index + 1}" }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+  tags = { Name = "private-subnet-${count.index + 1}" }
+}
