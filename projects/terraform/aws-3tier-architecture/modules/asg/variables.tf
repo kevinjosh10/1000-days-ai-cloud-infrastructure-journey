e6ee0d7bbab3af5a13627e6d4800cc7a959a888a
Kevin Joshua <kingkevinjosh7@gmail.com>
2026-07-31 18:02:03 +0530

    feat(asg): Define inputs for Auto Scaling web tier dependencies

---
+++ projects/terraform/aws-3tier-architecture/modules/asg/variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_id" { type = string }
+variable "public_subnet_ids" { type = list(string) }
+variable "alb_security_group_id" { type = string }
+variable "target_group_arn" { type = string }
